39a885dcc88cdf47d50149395ec1b2016af8d88e
coder-nayas <nayasbashaa@gmail.com>
2025-05-30 13:05:00 +0530

    Updated script.js

--- public/script.js
+++ public/script.js
@@ -41,14 +41,14 @@ if ('webkitSpeechRecognition' in window) {
     getAIResponse(transcript);
   };
 
-  micButton.addEventListener("click", () =>{
-    // to stop the ongoing speech before starting recognition
-    if(window.speechSynthesis.speaking){
+  micButton.addEventListener("click", () 
+    => {
+    if (window.speechSynthesis.speaking) {
     window.speechSynthesis.cancel();
   }
     recognition.start();
 });
-else {
+} else {
   outputText.innerText = "Speech recognition not supported.";
 }
 
